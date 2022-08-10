0f8ed6ce13aac99b3d6e0264e58f226976dde2b7
CodePellet <50582944+CodePellet@users.noreply.github.com>
2022-08-10 11:46:28 +0200

    Added jest package for unit tests
    Added first unit tests using the jest package. Added build config to not compile tests to production code

---
+++ src/test/test.ts
@@ -0,0 +1,27 @@
+import { AdbDeviceTracker } from "..";
+
+const tracker = new AdbDeviceTracker();
+
+test("Check if info message is of type string", () => {
+    tracker.on("info", message => {
+        console.log(message);
+        expect((typeof message === 'string')).toBeTruthy();
+    });
+    tracker.emit("info", "This is a info message");
+});
+
+test("Check if error is handled correctly", () => {
+    tracker.on("error", error => {
+        expect(error.message).toBe("ErrorMessage");
+        expect(error.code).toBe("ETESTERROR");
+        expect(error.name).toBe("ETESTERROR");
+    });
+    tracker.emit("error", { message: "ErrorMessage", name: 'ETESTERROR', code: 'ETESTERROR' });
+})
+
+test("Check if all adb device data is available", () => {
+    tracker.on("data", adbDevices => {
+        expect(adbDevices).toBeDefined();
+    })
+});
+
